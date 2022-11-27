d60c217b4eff09abda54840ac7dcdfd579e75a7a
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2022-11-27 19:12:55 +0330

    add : add three functions for get data

--- src/components/services/Service.js
+++ src/components/services/Service.js
@@ -1,3 +1,22 @@
-export default function Service(){
+import axios from "axios";
 
+export default function Service() {
+    const http = axios.create({
+        baseURL: 'https://new-api.coco.gl/dashboard/intw'
+    })
+
+    const getTopEngaged = async () => {
+        const response = await http.get('/top/engaged');
+        return response.data;
+    }
+
+    const getTopFollowers = async () => {
+        const response = await http.get('/top/followers');
+        return response.data;
+    }
+
+    const getDetailsApi = async (id) => {
+        const response = await http.get(`/detail/${id}`);
+        return response.data;
+    }
 }
