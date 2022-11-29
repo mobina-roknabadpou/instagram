2040a34728de3219ddcc8cd9cbfee19d79695b9e
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2022-11-29 13:35:54 +0330

    add: add handlerError function for image error finally complete list engaged

--- src/components/listPages/topEngaged/EngagedPages.js
+++ src/components/listPages/topEngaged/EngagedPages.js
@@ -16,11 +16,46 @@ export default function EngagedPages() {
         console.log(result);
         setEngaged(result);
     }
-    return(
-        <>
-            <div className='list'>
 
-            </div>
-        </>
-    );
+    const errorHandler = (event) => {
+        event.currentTarget.src ='https://img.icons8.com/ios/50/null/user-male-circle.png';
+        event.currentTarget.className = 'error';
+    }
+
+    const imageOnLoadHandler = (event) => {
+        console.log(
+            `The image with url of ${event.currentTarget.src} has been loaded`
+        );
+        if (event.currentTarget.className !== "error") {
+            event.currentTarget.className = "success";
+        }
+    };
+    return (<>
+        <div className='list'>
+            {engaged.map((item) => <div key={item.pageId} className='list-engaged'>
+                <div className='left'>
+                    <img className='profile' src={item.profilePicUrl}
+                         onError={errorHandler}
+                         style={{width: '40px'}}
+                    />
+                    <div className='identity'>
+                        <div className='instagram-id'>@{item.instagramId}</div>
+                        <div className='full-name'>{item.fullName}</div>
+                    </div>
+                </div>
+                <div className='right'>
+                    <div className='category'>
+                        <div className={item.parenIconUrl !== null && item.parentCategory !== null ? 'active' : 'none'}>
+                            <img src={item.parentIconUrl} style={{width:'15px'}}/>
+                            <span> {item.parentCategory}</span>
+                        </div>
+                        <div className={item.category === null || item.category === "" ? 'none' : 'active'}>{item.category}</div>
+                    </div>
+                    <div className='category-icon'>
+                        <SlOptionsVertical size={16}/>
+                    </div>
+                </div>
+            </div>)}
+        </div>
+    </>);
 }
