caa9470bb2be384a611164cf21f03170251b21d0
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2022-11-29 13:46:29 +0330

    add: add attribute to image

--- src/components/listPages/topEngaged/EngagedPages.js
+++ src/components/listPages/topEngaged/EngagedPages.js
@@ -36,6 +36,7 @@ export default function EngagedPages() {
                     <img className='profile' src={item.profilePicUrl}
                          onError={errorHandler}
                          style={{width: '40px'}}
+                         alt=""
                     />
                     <div className='identity'>
                         <div className='instagram-id'>@{item.instagramId}</div>
@@ -45,7 +46,9 @@ export default function EngagedPages() {
                 <div className='right'>
                     <div className='category'>
                         <div className={item.parenIconUrl !== null && item.parentCategory !== null ? 'active' : 'none'}>
-                            <img src={item.parentIconUrl} style={{width:'15px'}}/>
+                            <img src={item.parentIconUrl} style={{width:'15px'}}
+                                 alt=""
+                            />
                             <span> {item.parentCategory}</span>
                         </div>
                         <div className={item.category === null || item.category === "" ? 'none' : 'active'}>{item.category}</div>
