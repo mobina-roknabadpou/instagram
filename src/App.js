6b4deb9fb7a26fe968854839ec6d9ba94d4f0f5c
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2022-11-29 17:42:54 +0330

    use : use routers for translate pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './index.css';
-import ListPages from "./components/accounts/listpages/ListPages";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Pages from "./components/accounts/listpages/pages/Pages";
 import DetailPages from "./components/accounts/detailpages/DetailPages";
 
 function App() {
-  return (
-    <div className="App">
-        <EngagedPages/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Pages/>}/>
+                    <Route path='/pages' element={<Pages/>}/>
+                    <Route path='/detail/:userId' element={<DetailPages/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
