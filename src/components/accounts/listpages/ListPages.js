55f229f0b461399a4ababc009dbceed9ec2b2942
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2022-11-30 19:40:23 +0330

    change: change content

--- src/components/accounts/listpages/ListPages.js
+++ src/components/accounts/listpages/ListPages.js
@@ -2,7 +2,7 @@ import {SlOptionsVertical} from 'react-icons/sl';
 import {useNavigate} from "react-router-dom";
 import Header from "./pages/header/Header";
 
-export default function ListPages({data}) {
+export default function ListPages({data, title, loading}) {
     const navigate = useNavigate();
     const errorHandler = (event) => {
         event.currentTarget.src = 'https://img.icons8.com/ios/50/null/user-male-circle.png';
@@ -10,35 +10,56 @@ export default function ListPages({data}) {
     }
 
     return (<>
-        <div className='list' >
-            {data.map((item) => <div key={item.pageId} className='list-pages' onClick={() => navigate(`/detail/${item.instagramId}`)}>
-                <div className='left'>
-                    <img className='profile' src={item.profilePicUrl}
-                         onError={errorHandler}
-                         style={{width: '40px'}}
-                         alt=""
-                    />
-                    <div className='userid'>
-                        <div className='instagram-id'>@{item.instagramId}</div>
-                        <div className='full-name'>{item.fullName}</div>
-                    </div>
-                </div>
-                <div className='right'>
-                    <div className='category'>
-                        <div className={item.parenIconUrl !== null && item.parentCategory !== null ? 'active' : 'none'}>
-                            <img src={item.parentIconUrl} style={{width: '15px'}}
-                                 alt=""
-                            />
-                            <span> {item.parentCategory}</span>
+        <div className='list'>
+            <Header title={title}/>
+            {
+                loading ?
+                    <>
+                        <div className="lds-roller">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
                         </div>
-                        <div
-                            className={item.category === null || item.category === "" ? 'none' : 'active'}>{item.category}</div>
+                    </>
+                    :
+                    <div className='container'>
+                        {data.map((item) => <div key={item.pageId} className='list-pages'
+                                                 onClick={() => navigate(`/detail/${item.instagramId}`)}>
+                            <div className='left'>
+                                <img className='profile' src={item.profilePicUrl}
+                                     onError={errorHandler} alt=""/>
+                                <div className='userid'>
+                                    <div className='instagram-id'>@{item.instagramId}</div>
+                                    <div className='full-name'>{item.fullName}</div>
+                                </div>
+                            </div>
+                            <div className='right'>
+                                <div className='category'>
+                                    <div
+                                        className={item.parenIconUrl !== null && item.parentCategory !== null ? 'active' : 'none'}>
+                                        <img src={item.parentIconUrl} alt=""/>
+                                        <span className='parent-category'> {item.parentCategory}</span>
+                                    </div>
+                                    <div
+                                        className={item.category === null || item.category === "" ? 'none' : 'active'}>
+                                <span>
+                                     {item.category}
+                                </span>
+
+                                    </div>
+                                </div>
+                                <div className='category-icon'>
+                                    <SlOptionsVertical size={16}/>
+                                </div>
+                            </div>
+                        </div>)}
                     </div>
-                    <div className='category-icon'>
-                        <SlOptionsVertical size={16}/>
-                    </div>
-                </div>
-            </div>)}
+            }
         </div>
     </>);
 }
