47f5e7ba326170bbe57506ee1faa17a09c992d1f
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2022-11-30 19:41:54 +0330

    create : created header component

---
+++ src/components/accounts/listpages/pages/header/Header.js
@@ -0,0 +1,12 @@
+export default function Header({title}) {
+    return (
+        <>
+            <div className='headers'>
+                <h1>
+                    {title}
+                </h1>
+            </div>
+        </>
+
+    );
+}
